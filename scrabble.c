da83b9ebc7f466b0f80fcfc72820b62b63ef88c9
USArtist <79308981+USArtist@users.noreply.github.com>
2021-04-03 22:13:10 -0700

    Update scrabble.c

--- scrabble.c
+++ scrabble.c
@@ -2,12 +2,10 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-
-// Points assigned to each letter of the alphabet
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+// Points assigned
+int POINTS[] = {1, 3, 2, 1, 4, 2, 4, 8, 5, 1, 3, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
-
 int main(void)
 {
     // Get input words from both players
@@ -17,11 +15,9 @@ int main(void)
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
-
-    // TODO: Print the winner
+    // TODO: Print
 }
-
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    // TODO: return score for string
 }
